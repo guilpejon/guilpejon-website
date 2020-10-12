59187ec4d91802251c6d801135a5a29415633c94
Guilherme Pejon <guilpejon@gmail.com>
2020-10-12 01:38:47 -0300

    Added bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 import '../stylesheets/application'
+import './bootstrap_custom.js'
 import './home'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
