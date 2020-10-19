e7f437e47cd466a76bc85a5dc280916d744ed665
Guilherme Pejon <guilpejon@gmail.com>
2020-10-18 23:18:46 -0300

    Added default og:image

--- config/initializers/default_meta.rb
+++ config/initializers/default_meta.rb
@@ -1,7 +1,7 @@
 DEFAULT_META = {
   meta_title: 'GuilPejon',
   meta_description: 'Web Development and Art',
-  # meta_image: "logo.png",
+  meta_image: "guil.jpg",
   twitter_account: '@guilpejon',
   meta_type: 'website',
   meta_robots: 'index, follow'
