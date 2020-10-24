ae663c2e1aec235792be8b4238f7d07926bea5a4
Guilherme Pejon <guilpejon@gmail.com>
2020-10-24 17:44:41 -0300

    Added email template

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,12 @@
 class ContactsController < ApplicationController
   def send_email
     if verify_recaptcha(action: 'contact', minimum_score: 0.5)
-      ContactMailer.with(message: "teste").contact_email.deliver_later
+      ContactMailer.with(
+        name: params[:name],
+        email: params[:email],
+        subject: params[:subject],
+        message: params[:message],
+      ).contact_email.deliver_later
       flash.now[:error] = nil
       redirect_to root_path, notice: 'Message sent successfully!'
     else
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,10 @@
 class ContactMailer < ApplicationMailer
   def contact_email
     @message = params[:message]
+    @name = params[:name]
     mail(
       to: Rails.application.credentials.dig(:personal_email),
-      subject: 'Thanks for signing up for our amazing app',
+      subject: params[:subject],
       reply_to: params[:email]
     )
   end
